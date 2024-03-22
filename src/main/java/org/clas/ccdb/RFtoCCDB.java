532a6307fae64b98991ea67e1daab449a15298c2
Raffaella De Vita <devita@ge.infn.it>
2024-03-22 11:33:42 -0400

    graphical improvements

--- src/main/java/org/clas/ccdb/RFtoCCDB.java
+++ src/main/java/org/clas/ccdb/RFtoCCDB.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -14,6 +13,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jlab.groot.base.GStyle;
 import org.jlab.groot.data.GraphErrors;
+import org.jlab.groot.data.IDataSet;
 import org.jlab.groot.group.DataGroup;
 import org.jlab.groot.ui.TCanvas;
 import org.jlab.jnp.utils.options.OptionParser;
@@ -28,12 +28,14 @@ public class RFtoCCDB {
 
     private List<String> offsetsFile = null;
     private double threshold = 0.005;
+    private boolean update = true;
     private String fontName = "Arial";
     private DataGroup dg = new DataGroup(1,3);
 
-    public RFtoCCDB(List<String> file, double t) {
+    public RFtoCCDB(List<String> file, double t, boolean update) {
         this.offsetsFile = file;
         this.threshold = t;
+        this.update = update;
         
         GStyle.getAxisAttributesX().setTitleFontSize(18);
         GStyle.getAxisAttributesX().setLabelFontSize(14);
@@ -54,25 +56,31 @@ public class RFtoCCDB {
             offset.setTitleX("Run Number");
             offset.setTitleY("Offset (ns)");
             offset.setMarkerColor(3+i);
-            offset.setMarkerSize(3);
+            offset.setMarkerSize(4);
             dg.addDataSet(offset, 0);
             GraphErrors ccdb = new GraphErrors("RFccdb"+(i+1));
             ccdb.setTitleX("Run Number");
             ccdb.setTitleY("Offset (ns)");
-            ccdb.setMarkerColor(33+i);
-            ccdb.setMarkerSize(3);
+            ccdb.setMarkerColor(1);
+            ccdb.setMarkerSize(1);
             dg.addDataSet(ccdb, 0);
             GraphErrors delta = new GraphErrors("RFdelta"+(i+1));
             delta.setTitleX("Run Number");
-            delta.setTitleY("Delta (ns)");
+            delta.setTitleY("(Offset - CCDB) (ns)");
             delta.setMarkerColor(5+i);
-            delta.setMarkerSize(3);
+            delta.setMarkerSize(4);
             dg.addDataSet(delta, 1);
+            GraphErrors done = new GraphErrors("RFdone"+(i+1));
+            done.setTitleX("Run Number");
+            done.setTitleY("Offset (ns)");
+            done.setMarkerColor(1);
+            done.setMarkerSize(1);
+            dg.addDataSet(done, 1);
             GraphErrors sigma = new GraphErrors("RFsigma"+(i+1));
             sigma.setTitleX("Run Number");
-            sigma.setTitleY("Sigma (ns)");
+            sigma.setTitleY("#sigma (ns)");
             sigma.setMarkerColor(3+i);
-            sigma.setMarkerSize(3);
+            sigma.setMarkerSize(4);
             dg.addDataSet(sigma, 2);
         }
         System.out.println("\nProcessing file " + this.offsetsFile + " with threshold set to " + this.threshold);
@@ -137,8 +145,13 @@ public class RFtoCCDB {
                     last = run;
                 }
                 else {
-                    if(previous[0].isNew(threshold) || previous[1].isNew(threshold)) {
+                    if(previous[0].isNew(threshold) || previous[1].isNew(threshold) || this.update) {
                         this.write2CCDB(first, last, previous);
+                    }
+                    else {
+                        for(int i=0; i<2; i++) {
+                            dg.getGraph("RFdone"+(i+1)).addPoint(run, previous[i].delta, 0, previous[i].error);            
+                        }               
                     }
                     first = run;
                     last  = run;
@@ -174,8 +187,17 @@ public class RFtoCCDB {
     }
     
     public void plot() {
-        TCanvas vanvas = new TCanvas("RF", 800, 600);
-        vanvas.getCanvas().draw(dg);
+        int ncol = dg.getColumns();
+        int nrow = dg.getRows();
+        DataGroup dgn = new DataGroup(ncol, nrow);
+        for(int i=0; i<nrow*ncol; i++) {
+            for(IDataSet ds : dg.getData(i)) {
+                if(ds.getDataSize(0)>0)
+                    dgn.addDataSet(ds, i);
+            }
+        }
+        TCanvas canvas = new TCanvas("RF", 1200, 1400);
+        canvas.getCanvas().draw(dgn);
     }
 
     public class RF {
@@ -219,12 +241,14 @@ public class RFtoCCDB {
         // valid options for event-base analysis
         parser.setRequiresInputList(true);
         parser.addOption("-t", "0.005", "minimum offset variation (ns)");
+        parser.addOption("-r", "1", "disregard run ranges where new and old constants are within the threshold (0) or get range anyway (1)");
 
         parser.parse(args);
         List<String> files = parser.getInputList();
         double threshold = parser.getOption("-t").doubleValue();
+        boolean update   = parser.getOption("-r").intValue()==1;
         
-        RFtoCCDB rf = new RFtoCCDB(files, threshold);
+        RFtoCCDB rf = new RFtoCCDB(files, threshold, update);
                 
         try {
             rf.readOffsets();
